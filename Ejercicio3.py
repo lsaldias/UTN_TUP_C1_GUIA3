2ab519863743509d03407bfde1b0ebae94bc66ae
Abdul Emgert <abdul_ska@hotmail.com>
2023-09-03 23:53:15 -0300

    Clinica 1
    Agregar funcionalidad imprimir recetario

---
+++ Ejercicio3.py
@@ -0,0 +1,4 @@
+datos_paciente=input("Ingrese nombre completo del paciente y fecha de nacimiento 'en formato dd/mm/aaaa' ")
+datos_droga=input("Ingrese la medicacion a recetar: nombre, cantidad a administrar y modo de uso: ")
+
+print("Clinica -Nombre y direccion de la misma- Sr/sra: ", datos_paciente, " y su receta es: ", datos_droga)
