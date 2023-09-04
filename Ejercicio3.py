bcc5f4ac3ade598d1db3687e6e55e3cdd0929c1e
Abdul Emgert <abdul_ska@hotmail.com>
2023-09-04 00:07:20 -0300

    Modificacion punto 2
    Nueva funcionalidad de proximo turno

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,4 +1,8 @@
 datos_paciente=input("Ingrese nombre completo del paciente y fecha de nacimiento 'en formato dd/mm/aaaa' ")
 datos_droga=input("Ingrese la medicacion a recetar: nombre, cantidad a administrar y modo de uso: ")
+proximo_turno=input(("Ingrese la fecha del proximo turno 'en formato dd/mm y horario 24hs' "))
 
-print("Clinica -Nombre y direccion de la misma- Sr/sra: ", datos_paciente, " y su receta es: ", datos_droga)
+print("Clinica -Nombre y direccion de la misma- Sr/sra: ", datos_paciente, " , su receta es: ", datos_droga, "y el proximo turno sera el dia:" , proximo_turno)
+
+
+
